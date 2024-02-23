3d2e4becf80e622f60f6aad53d65580591f8b2ad
Sidharth Deleep <sidharthdeleep@gmail.com>
2024-02-23 16:28:44 +0530

    section7section8 and some blogeditor editor part, didnt do section 9

--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useContext, useRef } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import googleIcon from "../imgs/google.png";
@@ -6,15 +6,23 @@ import { Link } from "react-router-dom";
 import { Toaster, toast } from "react-hot-toast";
 import axios from "axios";
 import { storeInSession } from "../common/session";
-
+import { UserContext } from "../App";
+import { Navigate } from "react-router-dom"; 
 
 const UserAuthForm = ({ type }) => {
+
+  const authForm = useRef();
+
+  let { userAuth:{access_token}, setUserAuth  } = useContext(UserContext)
+
+  
   const userAuthThroughServer = (serverRoute, formData) => {
     axios
       .post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
       .then(({ data }) => {
         storeInSession("user", JSON.stringify(data))
-        console.log(sessionStorage);
+
+        setUserAuth(data)
       })
       .catch(({ response }) => {
         toast.error(response.data.error);
@@ -62,6 +70,9 @@ const UserAuthForm = ({ type }) => {
   };
 
   return (
+    access_token ?
+    <Navigate to="/"/>
+    :
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
         <Toaster />
