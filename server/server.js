acc1e061ac8f58d16be3b754f6446f42156cfb7d
architism <realarchit7@gmail.com>
2024-03-23 12:51:44 +0530

    getting profile details

--- server/server.js
+++ server/server.js
@@ -195,6 +195,21 @@ server.post("/search-users",(req,res)=>{
     })
 })
 
+server.post("/get-profile", (req,res) => {
+    let { username } = req.body;
+
+    User.findOne({ "personal_info.username": username })
+    .select("-personal_info.password -google_auth -updatedAt -blogs")
+    .then(user => {
+        return res.status(200).json(user)
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({ error: err.message })
+    })
+
+})
+
 server.post("/search-blogs",(req,res) =>{
     let {tag, page, query} = req.body;
     let findQuery;
